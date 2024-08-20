e9ffbadede86622468ee9dcd8f0e10157dc12097
Jorge Monge <jlo4321jl@gmail.com>
2024-08-20 09:15:53 -0600

    chore: Add authentication module to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { DatabaseModule } from './database/database.module';
+import { ConfigurationModule } from './configuration/configuration.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, DatabaseModule, ConfigurationModule],
   controllers: [AppController],
   providers: [AppService],
 })
