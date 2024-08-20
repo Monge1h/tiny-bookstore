46e1f1c8a61ccea4decda7663aefc3dc7cfc23a8
Jorge Monge <jlo4321jl@gmail.com>
2024-08-20 15:50:43 -0600

    chore: Add Swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'JWT-auth',
+    )
+    .setTitle('Tiny-store api')
+    .setDescription('Documentation for Tiny-store api')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document, {
+    swaggerOptions: { defaultModelsExpandDepth: -1, docExpansion: 'none' },
+  });
+
   await app.listen(3000);
 }
 bootstrap();
