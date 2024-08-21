f4185b256df8433a1771dc4ee56b43f96cdfb5df
Jorge Monge <jlo4321jl@gmail.com>
2024-08-20 20:15:42 -0600

    feat: Add dto validaion on local-auth.guard.ts

--- src/auth/guards/local-auth.guard.ts
+++ src/auth/guards/local-auth.guard.ts
@@ -1,5 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  ExecutionContext,
+  BadRequestException,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { LoginDto } from '../dto/login.dto';
+import { validate } from 'class-validator';
+import { plainToClass } from 'class-transformer';
 
 @Injectable()
-export class LocalAuthGuard extends AuthGuard('local') {}
+export class LocalAuthGuard extends AuthGuard('local') {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const body = request.body;
+
+    const loginDto = plainToClass(LoginDto, body);
+
+    const errors = await validate(loginDto);
+    if (errors.length > 0) {
+      const errorMessages = errors.map((error) => ({
+        property: error.property,
+        constraints: error.constraints,
+      }));
+
+      throw new BadRequestException({
+        message: 'Validation failed',
+        errors: errorMessages,
+      });
+    }
+
+    return (await super.canActivate(context)) as boolean;
+  }
+}
