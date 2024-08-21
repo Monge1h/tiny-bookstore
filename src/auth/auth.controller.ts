a310ec497b4850fb593a507626415b15e00ee722
Jorge Monge <jlo4321jl@gmail.com>
2024-08-20 23:18:43 -0600

    feat: Add API endpoint for user login with authentication to documentation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth.guard';
+import { ApiTags, ApiBody } from '@nestjs/swagger';
+import { LoginDto } from './dto/login.dto';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @UseGuards(LocalAuthGuard)
+  @ApiBody({ type: LoginDto })
   @Post('login')
   async login(@Request() req) {
     return this.authService.login(req.user);
