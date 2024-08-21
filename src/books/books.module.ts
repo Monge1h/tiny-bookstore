3d5285cb7d1b963d219c89aebe731ef60b183460
Jorge Monge <jlo4321jl@gmail.com>
2024-08-21 08:53:26 -0600

    feat: Add AuthModule to BooksModule, CartModule, and OrderModule

--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { BooksService } from './books.service';
 import { BooksController } from './books.controller';
 import { CloudinaryModule } from 'src/cloudinary/cloudinary.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [BooksController],
   providers: [BooksService],
-  imports: [CloudinaryModule],
+  imports: [CloudinaryModule, AuthModule],
 })
 export class BooksModule {}
--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [OrderController],
   providers: [OrderService],
 })
